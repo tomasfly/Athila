6b8b986cdf74abf0c7443092fe3ea201a8b7a291
Tomas Fleiderman <tomasfleiderman@xpertai.com>
2020-10-19 16:05:48 +0200

    update

---
+++ SMA10CrossSMA50.cs
@@ -0,0 +1,99 @@
+#region Using declarations
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Xml.Serialization;
+using NinjaTrader.Cbi;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
+using NinjaTrader.Data;
+using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
+using NinjaTrader.NinjaScript.Indicators;
+using NinjaTrader.NinjaScript.DrawingTools;
+#endregion
+
+//This namespace holds Strategies in this folder and is required. Do not change it. 
+namespace NinjaTrader.NinjaScript.Strategies
+{
+	public class SMA10CrossSMA50 : Strategy
+	{
+		private SMA SMA1;
+		private SMA SMA2;
+
+		protected override void OnStateChange()
+		{
+			if (State == State.SetDefaults)
+			{
+				Description									= @"Enter the description for your new custom Strategy here.";
+				Name										= "SMA10CrossSMA50";
+				Calculate									= Calculate.OnBarClose;
+				EntriesPerDirection							= 1;
+				EntryHandling								= EntryHandling.AllEntries;
+				IsExitOnSessionCloseStrategy				= true;
+				ExitOnSessionCloseSeconds					= 30;
+				IsFillLimitOnTouch							= false;
+				MaximumBarsLookBack							= MaximumBarsLookBack.TwoHundredFiftySix;
+				OrderFillResolution							= OrderFillResolution.Standard;
+				Slippage									= 0;
+				StartBehavior								= StartBehavior.WaitUntilFlat;
+				TimeInForce									= TimeInForce.Gtc;
+				TraceOrders									= false;
+				RealtimeErrorHandling						= RealtimeErrorHandling.StopCancelClose;
+				StopTargetHandling							= StopTargetHandling.PerEntryExecution;
+				BarsRequiredToTrade							= 20;
+				// Disable this property for performance gains in Strategy Analyzer optimizations
+				// See the Help Guide for additional information
+				IsInstantiatedOnEachOptimizationIteration	= true;
+			}
+			else if (State == State.Configure)
+			{
+				
+			}
+			else if (State == State.DataLoaded)
+			{				
+				SMA1				= SMA(Close, 10);
+				SMA2				= SMA(Close, 50);
+				AddChartIndicator (SMA1);
+				AddChartIndicator (SMA2);
+			}
+		}
+
+		protected override void OnBarUpdate()
+		{
+			if (BarsInProgress != 0) 
+				return;
+
+			if (CurrentBars[0] < 1)
+				return;
+
+			 // Set 1
+			if (CrossAbove(SMA1, SMA2, 1))
+			{
+				EnterLong (10);
+			}
+			
+			if (Position.MarketPosition == MarketPosition.Long) {
+				Print(SMA1[0]);
+			}
+			
+			
+			if (SMA1[0]<SMA1[1])
+			{
+				Print("time"+Time[0]);
+				Print("Exiting because SMA1 crossed"+SMA1[0]);
+				ExitLong ();
+			}
+			
+		}
+	}
+}
